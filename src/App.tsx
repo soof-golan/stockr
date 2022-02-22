d6f955c6afa203b36a5e9e87864026e4abb756ab
soof-golan <83900570+soof-golan@users.noreply.github.com>
2022-02-22 19:46:45 +0200

    Add trade and quote handling

--- src/App.tsx
+++ src/App.tsx
@@ -1,51 +1,55 @@
 import {useState} from 'react'
 import './App.css'
 
-const newAlpacaSocket = ({keyId = '', secret = ''}) => {
-  type AlpacaMessage = { T: string, msg: string, code?: number };
-
-  const ws = new WebSocket('wss://stream.data.alpaca.markets/v2/iex');
-  const send = (o: any) => ws.send(JSON.stringify(o));
-
-  const authenticate = () => {
-    console.log('Authenticating', keyId)
-    send({action: "auth", key: keyId, secret});
-  }
-  const handleError = (message: AlpacaMessage) => {
-    console.error(message)
-    ws.close()
-  }
-  const handlers: { [eventName: string]: (message: AlpacaMessage) => any } = {
-    'authenticated': (message: AlpacaMessage) => console.log('Connected successfully!'),
-    'connected': ({T}: AlpacaMessage) => (T === 'success') ? authenticate() : null,
-    "connection limit exceeded": handleError,
-    "auth timeout": handleError,
-    "auth failed": handleError,
-  }
-
-  ws.onmessage = (event: MessageEvent) => {
-    const messages: AlpacaMessage[] = JSON.parse(event.data);
-    console.log('Message:', messages)
-    messages.filter(message => message?.msg in handlers).map((message) => {
-      return handlers[message.msg](message);
-    })
-    messages.filter(messages => messages.T in tradeHandlers).map(message => handlers[message.T])
-  }
-
-  return ws;
-}
+type AlpacaMessage = { T: string, msg: string, code?: number };
+type AlpacaCredentials = { secret: string, keyId: string };
 
 class SocketController {
   private ws: Partial<WebSocket> = {};
+  private credentials?: AlpacaCredentials;
   private promisifyClose = async (ws: Partial<WebSocket>) => new Promise<void>(resolve => ws.onclose = (e) => resolve());
   private static instance: SocketController = new SocketController();
   static get = () => SocketController.instance;
 
+  private constructor() {  };
   send = (obj: {}) => this.ws?.send?.(JSON.stringify(obj));
 
-  private close = async () => {
+  handleError = (message: AlpacaMessage) => {
+    console.error(message)
+    this.close();
+  }
+
+  private authenticate = () => {
+    // @ts-ignore
+    const {keyId, secret} = this.credentials;
+    console.log('Authenticating', keyId)
+    this.send({action: "auth", key: keyId, secret});
+  }
+
+  private tradeHandlers: {[T: string]: Array<(m: AlpacaMessage) => null>} = {
+    q: [],
+    t: []
+  }
+
+  private handlers: { [eventName: string]: (message: AlpacaMessage) => any } = {
+    'authenticated': (message: AlpacaMessage) => console.log('Connected successfully!'),
+    'connected': ({T}: AlpacaMessage) => (T === 'success') ? this.authenticate(this) : null,
+    "connection limit exceeded": this.handleError,
+    "auth timeout": this.handleError,
+    "auth failed": this.handleError,
+  }
+
+  private onMessage = (event: MessageEvent) => {
+    const messages: AlpacaMessage[] = JSON.parse(event.data);
+    console.log('Message:', messages)
+    messages.filter(message => message?.msg in this.handlers).map((message) => this.handlers[message.msg](message));
+    messages.filter(messages => messages.T in this.tradeHandlers).map(message => this.tradeHandlers[message.T].map(f => f(message)));
+  }
+
+  private close = () => {
+    if (!this?.ws) return;
     const closed = this.promisifyClose(this.ws);
-    this?.ws?.close?.();
+    this.ws?.close?.();
     closed.then();
     this.ws.onclose = null;
     this.ws.onmessage = null;
@@ -53,15 +57,15 @@ class SocketController {
     this.ws.onopen = null;
   }
 
-  connect = ({keyId = '', secret = ''}) => {
-    this.close().then();
-    this.ws = newAlpacaSocket({keyId, secret});
+  connect = (credentials: AlpacaCredentials) => {
+    this.close();
+    this.credentials = credentials
+    this.ws = new WebSocket('wss://stream.data.alpaca.markets/v2/iex');
+    this.ws.onmessage = this.onMessage.bind(this);
   }
 
   subscribe = (ticker: string) => this.send({action: "subscribe", trades: [ticker], quotes: [ticker]});
   unsubscribe = (ticker: string) => this.send({action: "unsubscribe", trades: [ticker], quotes: [ticker]});
-  // onTrade = (handler) => this.tradeHandlers.push(handler);
-  // onQuote = (handler) => this.quoteHandlers.push(handler);
 }
 
 const useSocketController = () => SocketController.get()
