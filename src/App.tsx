c5f2c61b80798497c06e5f06606f23f3dab26d9b
soof-golan <83900570+soof-golan@users.noreply.github.com>
2022-03-12 14:42:14 +0200

    Improve SEO lighthouse score

--- src/App.tsx
+++ src/App.tsx
@@ -4,11 +4,24 @@ import Box from '@mui/material/Box';
 import {TradeTable} from "./TradeTable";
 import {ConnectionControl} from "./ConnectionControl";
 import {PromoSection} from "./PromoSection";
+import DocumentMeta from 'react-document-meta';
+
 
 function App() {
   const gridXs = 3;
+
+  const meta = {
+    title: "stockr",
+    description: "Real-time trade monitor",
+    canonical: "https://stockr.soofgolan.com",
+    meta: {
+      charset: 'utf-8',
+      keywords: 'stock,trade,monitor,websockt,websockts,realtime'
+    },
+  }
   return (
     <div className="App">
+      <DocumentMeta {...meta} />
       <header className="App-header">
         <h1>stockr</h1>
         <Box>
